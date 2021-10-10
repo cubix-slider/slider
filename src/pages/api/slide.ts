f207a7cc6aad63fe35db593c579fbd4e167e4273
jmaicaaan <johnmichaelubas.santos@gmail.com>
2021-10-10 16:25:28 +0800

    ♻️ refactor: Bring back guard pusher

--- src/pages/api/slide.ts
+++ src/pages/api/slide.ts
@@ -1,36 +1,33 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-// import Pusher from 'pusher';
+import Pusher from 'pusher';
 
-// import {
-//   ENV_PUSHER_API_KEY,
-//   ENV_PUSHER_APP_ID,
-//   ENV_PUSHER_CLUSTER,
-//   ENV_PUSHER_SECRET_KEY,
-// } from '../../constants/envs';
-// import { Events } from '../../constants/events';
+import {
+  ENV_PUSHER_API_KEY,
+  ENV_PUSHER_APP_ID,
+  ENV_PUSHER_CLUSTER,
+  ENV_PUSHER_SECRET_KEY,
+} from '../../constants/envs';
+import { Events } from '../../constants/events';
 
 export type Data = {};
 
-const handler = (req: NextApiRequest, res: NextApiResponse<Data>) => {
-  // console.log(1);
+const handler = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
   if (req.method !== 'POST') {
     return res.status(404).json({});
   }
-  // console.log(2);
 
-  // const pusher = new Pusher({
-  //   appId: ENV_PUSHER_APP_ID,
-  //   key: ENV_PUSHER_API_KEY,
-  //   secret: ENV_PUSHER_SECRET_KEY,
-  //   cluster: ENV_PUSHER_CLUSTER,
-  // });
+  const pusher = new Pusher({
+    appId: ENV_PUSHER_APP_ID,
+    key: ENV_PUSHER_API_KEY,
+    secret: ENV_PUSHER_SECRET_KEY,
+    cluster: ENV_PUSHER_CLUSTER,
+  });
 
 
-  // await pusher.trigger('slide-1', Events.SliderSlide, req.body);
+  await pusher.trigger('slide-1', Events.SliderSlide, req.body);
   
   res.status(200).json({ name: 'John Doe' });
-  // res.status(204).json({});
 };
 export default handler;
