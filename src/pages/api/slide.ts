791aab74eab7ada33af823394fa232aa3da88ead
jmaicaaan <johnmichaelubas.santos@gmail.com>
2021-10-10 16:17:10 +0800

    ♻️ refactor: Test

--- src/pages/api/slide.ts
+++ src/pages/api/slide.ts
@@ -3,22 +3,22 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 
 // import Pusher from 'pusher';
 
-import {
-  ENV_PUSHER_API_KEY,
-  ENV_PUSHER_APP_ID,
-  ENV_PUSHER_CLUSTER,
-  ENV_PUSHER_SECRET_KEY,
-} from '../../constants/envs';
-import { Events } from '../../constants/events';
+// import {
+//   ENV_PUSHER_API_KEY,
+//   ENV_PUSHER_APP_ID,
+//   ENV_PUSHER_CLUSTER,
+//   ENV_PUSHER_SECRET_KEY,
+// } from '../../constants/envs';
+// import { Events } from '../../constants/events';
 
 export type Data = {};
 
 const handler = (req: NextApiRequest, res: NextApiResponse<Data>) => {
-  console.log(1);
-  if (req.method !== 'POST') {
-    return res.status(404);
-  }
-  console.log(2);
+  // console.log(1);
+  // if (req.method !== 'POST') {
+  //   return res.status(404);
+  // }
+  // console.log(2);
 
   // const pusher = new Pusher({
   //   appId: ENV_PUSHER_APP_ID,
@@ -30,6 +30,7 @@ const handler = (req: NextApiRequest, res: NextApiResponse<Data>) => {
 
   // await pusher.trigger('slide-1', Events.SliderSlide, req.body);
   
-  res.status(204).json({});
+  res.status(200).json({ name: 'John Doe' });
+  // res.status(204).json({});
 };
 export default handler;
